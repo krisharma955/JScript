f47cc8550e45d51b95f3f8853f146353f6b706fa
Krish Sharma <krishsharma9005@gmail.com>
2025-06-06 09:23:32 +0530

    fxns

--- 03_basics.js
+++ 03_basics.js
@@ -65,3 +65,76 @@ function returnSecondValue(getArray) { //? use getArray as reference
 console.log(returnSecondValue(arr)); //* 300
 console.log(returnSecondValue([10,20,30,40])); //? passing direct array
 
+// Scopes in JS
+//! Problem with var
+if(true) {
+    let a = 1
+    const b = 2
+    var c = 3
+}
+////console.log(a); //* ERROR => which is correct because a is defined inside the if block, it can only be used inside that block of code
+////console.log(b); //* ERROR
+console.log(c); //* 3 => it can be used outside the block of code it is declared (PROBLEM) => Hence, let is more preferred
+
+//! Global Scope => A variable declared outside any function or block. It can be accessed anywhere in the code.
+let x = 10
+function printx() {
+    console.log(x) //* x is a global variable and can be accessed
+}
+printx() //* 10
+
+//! Local Scope => A variable declared inside a function or block. It can only be accessed there.
+function printy() {
+    let y = 10 //* can only be used inside this block i.e this fxn
+}
+////console.log(y) //* error => y is not defined => y is a local variable and can't be accessed globally
+
+//! Nested Scopes closure
+function one() {
+    const username = "thor"
+    function two() {
+        const website = "youtube"
+        console.log(username); //* thor => username has a global scope for this fxn and hence it can be accessed    
+    }
+    //console.log(website); //* throwing error bcz website has a local scope inside fxn two and not be accessed anywhere else
+    two()
+}
+one()
+
+if(true) {
+    const username = "antman"
+    if(username === "antman") {
+        const website = "X"
+        console.log(username + website); //* antmanX    
+    }
+    ////console.log(website); //* Error
+    
+}
+////console.log(username); //* Error
+
+//! Important
+console.log(addOne(2)) //* 3
+function addOne(num) { //* FUNCTION
+    return num+1
+}
+
+////console.log(addTwo(3)) //? error => Cannot access 'addTwo' before initialization
+const addTwo = function(num) { //* FUNCTION => it's also a EXPRESSION, a different method to declare a fxn => declaring a fxn and simultanously a variable is holding it
+    return num+2
+}
+
+// this & Arrow Functions
+//! this function
+const webuser = {
+    username: "krish",
+    price: 999,
+    welcomeMsg: function() {
+        console.log(`${this.username} welcome to website`) //? while referring to current context (present time) (this) is used, It tells about current values
+        console.log(this) //* { username: 'krish', price: 999, welcomeMsg: [Function: welcomeMsg] }
+    }
+}
+console.log(webuser.welcomeMsg()); //* krish welcome to website
+webuser.username = "blaze" //* context changed and now according to the current context username: "blaze"
+console.log(webuser.welcomeMsg()); //* blaze welcome to website
+//* { username: 'blaze', price: 999, welcomeMsg: [Function: welcomeMsg] }
+console.log(this) //* {}
